e6390db305de511cc7149bb16221b60f5cfce449
Lfu001 <goodspeed913@gmail.com>
2023-10-29 18:53:50 +0900

    Initial Commit

---
+++ tears/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tears
+//
+//  Created by Yuto Hirai on 2023/10/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
