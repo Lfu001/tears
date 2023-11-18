c86a3bd3f6b9113c2f1ad5d41fca3d5385dac02f
Lfu001 <goodspeed913@gmail.com>
2023-11-18 20:54:02 +0900

    refactor: add tears_assert()

---
+++ tears/utils/DebugUtil.hpp
@@ -0,0 +1,34 @@
+//
+//  DebugUtil.hpp
+//  tears
+//
+//  Created by Lfu001 on 2023/11/18.
+//  Copyright © 2023 tears team. All rights reserved.
+//
+
+#ifndef DebugUtil_h
+#define DebugUtil_h
+
+#include <csignal>
+#include <iostream>
+
+class DebugUtil {
+public:
+    /// raise signal
+    static void raiseSignal() { std::raise(SIGINT); }
+};
+
+/// validates the condition in debug builds and breaks if the condition is false (and then you can
+/// continue executing the program). In a release build, it does nothing.
+#ifdef DEBUG
+#    define tears_assert(condition)                                                          \
+        if (!(condition)) {                                                                  \
+            printf("Assertion failed: %s in file %s: %d\n", #condition, __FILE__, __LINE__); \
+            DebugUtil::raiseSignal();                                                        \
+        }                                                                                    \
+        ((void)0)
+#else
+#    define tears_assert(condition) ((void)0)
+#endif
+
+#endif /* DebugUtil_h */
