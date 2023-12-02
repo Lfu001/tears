6b18c884fbd5913ee61bfda247c928a6a1739566
Lfu001 <82690385+Lfu001@users.noreply.github.com>
2023-12-02 18:24:40 +0900

    feat: support no break mode in tears_assert() (#7)
    * fix: move DebugUtil into tears namespace
    
    * feat: add no break mode to tears_assert()

--- tears/utils/DebugUtil.hpp
+++ tears/utils/DebugUtil.hpp
@@ -12,30 +12,63 @@
 #include <csignal>
 #include <iostream>
 
-/// Variable template to check if the type is a `std::unique_ptr`
+namespace tears {
+
+using namespace std;
+
+/// variable template to check if the type is a `std::unique_ptr`
 template<typename T>
 constexpr bool is_unique_ptr_v = false;
-/// Variable template to check if the type is a `std::unique_ptr`
+/// variable template to check if the type is a `std::unique_ptr`
 template<typename T, typename Deleter>
 constexpr bool is_unique_ptr_v<std::unique_ptr<T, Deleter>> = true;
 
+/// a utility available only on debug build
 class DebugUtil {
+private:
+    /// flag for disable execution pause on `tears_assert()` failed
+    inline static bool noBreakMode = false;
+
 public:
-    /// raise signal
-    static void raiseSignal() { std::raise(SIGINT); }
+    /// pause execution and prints failed check, file name, line and function
+    static void pauseExecution(
+        const char* condition,
+        const char* fileName,
+        int line,
+        const char* functionName) {
+        if (!noBreakMode) {
+            cout << "Assertion failed: '" << condition << "' in file " << fileName << ":" << line
+                 << endl
+                 << "\tin " << functionName << endl;
+
+            /// If the execution pauses here repeatedly and you want to skip the pause triggered by
+            /// `tears_assert()` on this run, enter the following code into the lldb console and
+            /// continue the execution.
+            /// `(lldb) expr noBreakMode = true`
+            std::raise(SIGTRAP);
+        }
+    }
 };
+
+#ifdef _MSC_VER
+#    define __TEARS_FUNCTION__ __FUNCTION__
+#elif defined(__clang__) || defined(__GNUC__)
+#    define __TEARS_FUNCTION__ __PRETTY_FUNCTION__
+#else
+#    define __TEARS_FUNCTION__ __func__
+#endif
 
 /// validates the condition in debug builds and breaks if the condition is false (and then you can
 /// continue executing the program). In a release build, it does nothing.
 #ifdef DEBUG
-#    define tears_assert(condition)                                                          \
-        if (!(condition)) {                                                                  \
-            printf("Assertion failed: %s in file %s: %d\n", #condition, __FILE__, __LINE__); \
-            DebugUtil::raiseSignal();                                                        \
-        }                                                                                    \
-        ((void)0)
+#    define tears_assert(condition) \
+        (condition)                 \
+            ? ((void)0)             \
+            : DebugUtil::pauseExecution(#condition, __FILE__, __LINE__, __TEARS_FUNCTION__)
 #else
 #    define tears_assert(condition) ((void)0)
 #endif
+
+}    // namespace tears
 
 #endif /* DebugUtil_hpp */
