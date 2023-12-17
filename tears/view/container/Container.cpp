defe1e3625f328bfd6ff1f5c5db3055c950a1915
Lfu001 <goodspeed913@gmail.com>
2023-12-17 17:59:15 +0900

    feat: add HStack and VStack

---
+++ tears/view/container/Container.cpp
@@ -0,0 +1,16 @@
+//
+//  Container.cpp
+//  tears
+//
+//  Created by Lfu001 on 2023/12/17.
+//  Copyright © 2023 tears team. All rights reserved.
+//
+
+#include "Container.hpp"
+
+namespace tears {
+
+// destructor
+Container::~Container() {}
+
+}    // namespace tears
---
+++ tears/view/container/VStack.hpp
@@ -0,0 +1,37 @@
+//
+//  VStack.hpp
+//  tears
+//
+//  Created by Lfu001 on 2023/12/17.
+//  Copyright © 2023 tears team. All rights reserved.
+//
+
+#ifndef VStack_hpp
+#define VStack_hpp
+
+#include "Container.hpp"
+
+namespace tears {
+
+class VStack: public Container {
+protected:
+    /// default constructor
+    VStack() = delete;
+    /// initializer
+    void initialize();
+
+public:
+    /// constructor (with child views)
+    /// by default, children will be ordered vertically
+    /// @param aChildren child views where child is subclass of `View`
+    template<class... Views>
+    VStack(Views&&... aChildren): Container(forward<Views>(aChildren)...) {
+        initialize();
+    }
+    /// destructor
+    virtual ~VStack();
+};
+
+}    // namespace tears
+
+#endif /* VStack_hpp */
